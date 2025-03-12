724bfcee200d75caf1f556bc38968bb482206a07
Gabriel <Gb-rodrigues7>
2025-03-11 21:47:17 -0300

    commit idade(incompleto)

---
+++ Atividades/Atividade Calculadora/calculadora-projeto/calculadora.js
@@ -0,0 +1,25 @@
+// soma
+
+function soma(a, b) {
+    return a + b;
+}
+
+// subtração
+
+function subtracao(a, b){
+    return a - b;
+}
+
+// multiplicação
+
+function multiplicacao(a, b){
+    return a * b;
+}
+
+// divisão
+
+function divisao(a, b){
+    return a / b;
+}
+
+export {soma, subtracao, multiplicacao, divisao};
---
+++ Atividades/Atividade Idade/index.js
@@ -0,0 +1,8 @@
+import moment from './node_modules/moment';
+
+function calcularIdade(anoNascimento) {
+    // implemente a lógica para calcular a idade
+    }
+    const anoNascimento = 1990;
+    const idade = calcularIdade(anoNascimento);
+    console.log(`Idade: ${idade} anos`);
