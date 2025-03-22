5ab18accf42af5260402b8fb78e661454a8320a4
Gabriel <Gb-rodrigues7>
2025-03-21 21:43:57 -0300

    Commit Perfil

--- 05-fundamentos-react-native/App.js
+++ 05-fundamentos-react-native/App.js
@@ -5,6 +5,7 @@ import PrimeiroComponente from './componentes/PrimeiroComponente';
 import SegundoComponente from './componentes/SegundoComponente';
 import TerceiroComponente from './componentes/terceiroComponente';
 import JavaScriptCompomente from './componentes/JavaScriptCompomente';
+import Perfil from './componentes/Perfil';
 
 export default function App() {
   return (
@@ -23,6 +24,27 @@ export default function App() {
       <Text/>
 
       <JavaScriptCompomente/>
+
+      <Text/>
+      
+      <Perfil
+        nome = "Gabriel"
+        idade = {20}
+        email = "gabriel@gmail.com"
+      
+      />
+      <Perfil
+        nome = "Pedro"
+        idade = {26}
+        email = "Pedro@gmail.com"
+      
+      />
+      <Perfil
+        nome = "Cabral"
+        idade = {52}
+        email = "Cabral@gmail.com"
+      
+      />
 
     </View>
   );
---
+++ 05-fundamentos-react-native/componentes/Perfil.jsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Perfil(props) {
+
+    console.log(props)
+
+
+  return (
+    <View style = {styles.container}>
+      <Text style={{ fontsize : 40 }}>Perfil</Text>
+      <Text style={{ fontsize : 40 }}>Nome: {props.nome}</Text>
+      <Text style={{ fontsize : 40 }}>Idade: {props.idade}</Text>
+      <Text style={{ fontsize : 40 }}>Idade: {props.email}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'aqua'
+    }
+})
