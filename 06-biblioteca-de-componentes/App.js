b7bd53f36b950affd1688c2d9d4e1930765b16c3
Gabriel <Gb-rodrigues7>
2025-04-01 21:24:34 -0300

    Commit Biblioteca de componentes

---
+++ 06-biblioteca-de-componentes/App.js
@@ -0,0 +1,64 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { PaperProvider, Card, Title, Paragraph, Divider, Button } from 'react-native-paper';
+
+export default function App() {
+
+  const lista = ["uva", "Maça", "Banana", "Laranja"]
+  return (
+    <PaperProvider>
+      <View style={styles.container}>
+        <ScrollView>
+          <StatusBar style="auto" />
+
+
+            <Button mode='contained' onPress={() => alert("Clicou")}>Clique AQUI</Button>
+            <Button mode='conteined-tonal' onPress={() => alert("Clicou")}>Clique AQUI</Button>
+            <Button mode='outlined' onPress={() => alert("Clicou")}>Clique AQUI</Button>
+            <Button mode='text' onPress={() => alert("Clicou")}>Clique AQUI</Button>
+            <Button icon="camera" mode='conteined-tonal' onPress={() => alert("Clicou")}>Clique AQUI</Button>
+
+
+            <Text>Uva</Text>
+            <Divider />
+            <Text>Maça</Text>
+            <Divider />
+            <Text>Banana</Text>
+            <Divider />
+            <Text>Laranja</Text>
+            <Divider />
+
+
+            <Text variant='titleLarge'> Um texto qualquer</Text>
+            <Text variant='headLineLarge'>Um texto qualquer</Text>
+            <Text variant='displayLarge'>Um texto qualquer</Text>
+
+            <Card>
+              <Card.Content>
+                <Title> Título do Card</Title>
+                <Paragraph>Um paragrafo qualquer Um paragrafo qualquer Um paragrafo qualquer Um paragrafo qualquer Um paragrafo qualquer Um paragrafo qualquer Um paragrafo qualquer Um paragrafo qualquer Um paragrafo qualquer Um paragrafo qualquer Um paragrafo qualquer</Paragraph>
+              </Card.Content>
+              <Card.Cover source={{uri: 'https://i.pinimg.com/236x/9b/62/3d/9b623d45aa887148115efb58fa428b6d.jpg' }}/>
+            </Card>
+
+        </ScrollView>
+      </View>
+    </PaperProvider>
+
+
+
+
+
+
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
